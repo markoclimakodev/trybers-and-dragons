d0f54849ccb0eedb3c33dac53b8e1c13b0d30fd9
markoclimako <mkclimako@gmail.com>
2023-11-29 14:10:48 -0300

    starting project

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 function getRandomInt(min: number, max: number) {
+  
   const newMin = Math.ceil(min);
   const newMax = Math.floor(max);
   return Math.floor(Math.random() * (newMax - newMin)) + newMin;
